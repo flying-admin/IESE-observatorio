cecae57c9336ecbe591fad7f111a7d6c4db2b402
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-09-18 06:57:17 +0200

    header scroll styles

--- source/assets/javascript/all.js
+++ source/assets/javascript/all.js
@@ -17,5 +17,14 @@ $(window).on("resize", function(){
 });
 
 $(window).on("scroll", function(){
+  var scrollY = $(this).scrollTop();
 
+  var header = $('.header');
+  var headerOffset = 0;
+  if (scrollY > headerOffset && !header.hasClass('header_sticky')){
+    header.addClass('header_sticky');
+  }
+  else if(scrollY <= headerOffset && header.hasClass('header_sticky')) {
+    header.removeClass('header_sticky');
+  }
 });

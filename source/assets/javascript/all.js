e6925e3001eaa108807e85ab4904e16ccc55c4eb
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-10-17 16:27:46 +0200

    fix responsive menu

--- source/assets/javascript/all.js
+++ source/assets/javascript/all.js
@@ -39,6 +39,13 @@ $(window).on("load", function(){
     $(this).toggleClass('open');
     $('.mobile_menu_overlay').toggleClass('open');
     $('.mobile_menu').toggleClass('open');
+    if(!$('.mobile_menu').is('.open')) {
+      $('.mobile_menu_content').fadeOut('fast');
+    } else {
+      setTimeout(function(){
+        $('.mobile_menu_content').fadeIn();
+      }, 500);
+    }
   });
 
 

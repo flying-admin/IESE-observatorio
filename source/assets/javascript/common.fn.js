8a0400119b2ff493036117c5fbc652ff3df6b92b
María Peña Jaquete <maria.pena.jaquete@gmail.com>
2018-11-26 20:13:34 +0100

    new js structure

---
+++ source/assets/javascript/common.fn.js
@@ -0,0 +1,16 @@
+/******************************************/
+/* DEPENDENCIAS                           */
+/******************************************/
+//= require ./dependencies/jquery-3.3.1.min.js
+//= require ./dependencies/filesaver.min.js
+
+/******************************************/
+/* PLUGINS                                */
+/******************************************/
+//= require ./plugins/animations.js
+//= require ./plugins/bodyScrollLock.js
+
+/******************************************/
+/* CUSTOM                                */
+/******************************************/
+//= require ./custom/common.js
